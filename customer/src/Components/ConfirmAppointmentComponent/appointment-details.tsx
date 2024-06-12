d0a752ea731481ee3cac703b7a16daf43e8e417b
Anuradha00Herath <herathanuradha03@gmail.com>
2024-06-13 04:37:40 +0530

    update make appointment and confirm appointment page

--- customer/src/Components/ConfirmAppointmentComponent/appointment-details.tsx
+++ customer/src/Components/ConfirmAppointmentComponent/appointment-details.tsx
@@ -21,9 +21,15 @@ export function AppointmentDetails() {
           borderRadius: 8,
           display: "flex",
           boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+          flexDirection: "row",
+          width: "100%",
         }}
       >
-        <div style={{ flex: 1, margin: "0 10px" }}>
+        <div
+          style={{
+            width: "50%",
+          }}
+        >
           <h2
             style={{
               textAlign: "center",
@@ -40,80 +46,223 @@ export function AppointmentDetails() {
               borderRadius: "50%",
             }}
           ></div>
-          <h3>Amal Perera</h3>
+          <h3
+            style={{
+              textAlign: "center",
+            }}
+          >
+            Amal Perera
+          </h3>
           <table
             style={{
-              width: "100%",
+              width: "70%",
               marginBottom: "20px",
               border: "1px solid #e0e0e0",
               borderCollapse: "collapse",
+              margin: "0 auto 10px",
             }}
           >
-            <tbody>
+            <tbody
+              style={
+                {
+                  //width:"50%"
+                }
+              }
+            >
               <tr>
-                <td>Service Name</td>
-                <td>Hair Cut</td>
+                <td
+                  style={{
+                    border: "1px solid #e0e0e0",
+                    padding: "8px",
+                  }}
+                >
+                  Service Name
+                </td>
+                <td
+                  style={{
+                    border: "1px solid #e0e0e0",
+                    padding: "8px",
+                  }}
+                >
+                  Hair Cut
+                </td>
               </tr>
               <tr>
-                <td>Price</td>
-                <td>LKR500</td>
+                <td
+                  style={{
+                    border: "1px solid #e0e0e0",
+                    padding: "8px",
+                  }}
+                >
+                  Price
+                </td>
+                <td
+                  style={{
+                    border: "1px solid #e0e0e0",
+                    padding: "8px",
+                  }}
+                >
+                  LKR500
+                </td>
               </tr>
               <tr>
-                <td>Date</td>
-                <td>4th January 2024</td>
+                <td
+                  style={{
+                    border: "1px solid #e0e0e0",
+                    padding: "8px",
+                  }}
+                >
+                  Date
+                </td>
+                <td
+                  style={{
+                    border: "1px solid #e0e0e0",
+                    padding: "8px",
+                  }}
+                >
+                  4th January 2024
+                </td>
               </tr>
               <tr>
-                <td>Time</td>
-                <td>13:30 - 14:00</td>
+                <td
+                  style={{
+                    border: "1px solid #e0e0e0",
+                    padding: "8px",
+                  }}
+                >
+                  Time
+                </td>
+                <td
+                  style={{
+                    border: "1px solid #e0e0e0",
+                    padding: "8px",
+                  }}
+                >
+                  13:30 - 14:00
+                </td>
               </tr>
             </tbody>
           </table>
         </div>
-        <div className="your-details">
-          <h2>Your Details</h2>
-          <form style={{
+        <div style={{
+            width:1,
+            backgroundColor:"black"
+        }}></div>
+        <div
+          style={{
+            width: "50%",
             display: "flex",
-            flexDirection: "column"
+            justifyContent: "center",
           }}
-            onSubmit={(e) => {
-              e.preventDefault();
-              handleConfirmBooking();
+        >
+          <div
+            style={{
+              width: "80%",
             }}
           >
-            <label>
-              Your Name
+            <h2
+              style={{
+                textAlign: "center",
+              }}
+            >
+              Your Details
+            </h2>
+            <form
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleConfirmBooking();
+              }}
+            >
+              <label
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                }}
+              >
+                Your Name
+              </label>
               <input
+                style={{
+                  padding: "8px",
+                  marginTop: "5px",
+                  border: "none",
+                  borderBottom: "1px solid black",
+                  outline: "none",
+                  marginBottom: "10px",
+                }}
                 type="text"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
               />
-            </label>
-            <label>
-              Enter Contact Number
+              <label
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                }}
+              >
+                Enter Contact Number
+              </label>
               <input
+                style={{
+                  padding: "8px",
+                  marginTop: "5px",
+                  border: "none",
+                  borderBottom: "1px solid black",
+                  outline: "none",
+                  marginBottom: "10px",
+                }}
                 type="text"
                 value={contactNumber}
                 onChange={(e) => setContactNumber(e.target.value)}
               />
-            </label>
-            <label>
-              Enter Email Address
+              <label
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                }}
+              >
+                Enter Email Address
+              </label>
               <input
+                style={{
+                  padding: "8px",
+                  marginTop: "5px",
+                  border: "none",
+                  borderBottom: "1px solid black",
+                  outline: "none",
+                  marginBottom: "10px",
+                }}
                 type="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
-            </label>
-            <button style={{
-                padding: "10px",
-                backgroundColor: "#333",
-                color: "white",
-                border: "none",
-                borderRadius: "4px",
-                cursor: "pointer",
-                alignSelf: "center"
-            }} type="submit">Confirm Booking</button>
-          </form>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row-reverse",
+                }}
+              >
+                <button
+                  style={{
+                    height: 40,
+                    width: 150,
+                    backgroundColor: "#333",
+                    color: "white",
+                    border: "none",
+                    cursor: "pointer",
+                    alignSelf: "center",
+                  }}
+                  type="submit"
+                >
+                  Confirm Booking
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
